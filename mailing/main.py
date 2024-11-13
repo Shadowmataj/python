001f2ec68a9b6f2b97f83a8e11f05807e62abd26
Shadowmataj <christianmataj1@gmail.com>
2024-11-13 17:20:54 -0600

    send emails

---
+++ mailing/main.py
@@ -0,0 +1,28 @@
+#SIMPLE MAIL TRANSFER PROTOCOL
+import smtplib
+import datetime as dt
+import random
+
+APP_PASSWORD = "ytrdilgxckajmouv"
+
+my_email = "christianmataj1@gmail.com"
+test_email = "mata_juarez@hotmail.com"
+
+def send_email(quote):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=APP_PASSWORD)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=test_email,
+            msg=f"Subject: The motivational quote of the day\n\n{quote}")
+
+
+with open("quotes.txt") as quotes_file:
+    quotes = quotes_file.readlines()
+
+today = dt.datetime.now()
+weekday = today.weekday()
+if weekday == 2:
+    quote_of_the_day = random.choice(quotes)
+    send_email(quote_of_the_day)
